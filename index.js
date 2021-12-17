1917a6ebda3616f0e31d91eab78817e74322b979
SeherSaylik <sehersaylik@users.noreply.github.com>
2021-12-17 22:30:13 +0300

    conditions are completed index.js

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 const isCreditCardNumberValid = (cardNumber) => {
-
+    let status= true
+    let counter= 0
+    let sumNumbers= 0
+    const strCardNumber=String(cardNumber)
+    const length=strCardNumber.length
+    for (let index = 0; index < length; index++) {
+        let eachString= cardNumber.slice(index,index+1)
+        let checkInt=parseInt(eachString)
+        if(eachString==="-"){
+        }else if(isNaN(checkInt)){
+          status=false
+        }else{
+          counter=counter+1
+          sumNumbers= checkInt+ sumNumbers
+        }
+    }
+    if(counter!=16 || sumNumbers<16){
+      status=false
+    }
+    if((cardNumber.slice(length-1,length))%2!=0){
+      status=false
+    }
+    console.log(status)
 }
+isCreditCardNumberValid("11213234-123-23-238")
